564fb21d3ad7eb5ee07716e821f653706aace18a
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2023-08-26 11:34:36 -0700

    Small fixes

--- Assets/Scripts/RespawnController.cs
+++ Assets/Scripts/RespawnController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,12 +9,16 @@ public class RespawnController : MonoBehaviour
 
     private Vector2 startPos;
 
+    private Vector3 startRot;
+
 
     private SpriteRenderer spriteRenderer;
 
     private Rigidbody2D playerRB;
 
-    public  ParticleController particleController;
+    public ParticleController particleController;
+
+    public MovementController movementController;
 
     [SerializeField] private float waitToRespawn;
 
@@ -25,6 +30,7 @@ public class RespawnController : MonoBehaviour
         //Find the RigidBody2D on this game object
         playerRB = GetComponent<Rigidbody2D>();
 
+        movementController = GetComponent<MovementController>();
     }
 
     // Start is called before the first frame update
@@ -56,18 +62,28 @@ IEnumerator Respawn(float duration)
     {
         //disable the sprite renderer
         spriteRenderer.enabled = false;
+
         //immediately freeze the position so player stops moving
         playerRB.constraints = RigidbodyConstraints2D.FreezePositionX;
+        
         //stop all velocity for the player
         playerRB.velocity = new Vector2(0,0);
 
         yield return new WaitForSeconds(duration);
+        
         //after wait time has passed, reset the player position to the startPos
         transform.position = startPos;
+
+        ////reset player rotation
+        //Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+        //transform.rotation = Quaternion.Euler(rotator);
+
         //re-enable the sprite renderer
         spriteRenderer.enabled = true;
+        
         //un freeze all contraints on the rigidbody2D
         playerRB.constraints = RigidbodyConstraints2D.None;
+        
         // freeze the Z rotation again
         playerRB.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
