cab2ba8e03c474a2bd198ea5480a1d14492b580d
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2023-08-27 17:00:14 -0700

    Completion of: Unity 2D TELEPORT SYSTEM | Unity 2D Platformer Tutorial #12

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 
 public class PortalController : MonoBehaviour
 {
 
     [SerializeField] private Transform destination; //other portal
+
     private GameObject player;
+
+    private Animation anim;
+
+    private Rigidbody2D playerRB;
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        anim = player.GetComponent<Animation>();
+        playerRB = player.GetComponent<Rigidbody2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -20,10 +28,51 @@ public class PortalController : MonoBehaviour
 
             if(Vector2.Distance(player.transform.position, transform.position) > 0.3)
             {
-                // Only teleport player once within the target distance from the portal
-                player.transform.position = destination.transform.position;
+                StartCoroutine(PortalIn());
             }
             
+        }
+    }
+
+    IEnumerator PortalIn()
+    {
+        // disable RB as soon as player touched portal
+        playerRB.simulated = false;
+
+        //play the portal in animation
+        anim.Play("Portal_IN");
+
+        //trigger move into portal
+        StartCoroutine(MoveIntoPortal());
+
+        //wait
+        yield return new WaitForSeconds(0.5f);
+
+        // Only teleport player once within the target distance from the portal
+        player.transform.position = destination.transform.position;
+
+        //reset the velocity of the player so that we don't come out of portals with same velocity as we went in
+        playerRB.velocity = Vector2.zero;
+
+        // play the portal out animation
+        anim.Play("Portal_OUT");
+
+        //wait
+        yield return new WaitForSeconds(0.5f);
+
+        //re-activate RB component
+        playerRB.simulated = true;
+    }
+
+    IEnumerator MoveIntoPortal()
+    {
+        float timer = 0;
+
+        while (timer < 0.5f)
+        {
+            player.transform.position = Vector2.MoveTowards(player.transform.position, transform.position, 3 * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
         }
     }
 }
